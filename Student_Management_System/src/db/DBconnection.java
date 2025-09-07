c7238a3cb71691bf0f04d174840f1f53e5caeb5f
manju2512 <manjunath251203@gmail.com>
2025-09-07 11:10:22 +0530

    DBconnection.java

--- Student_Management_System/src/db/DBconnection.java
+++ Student_Management_System/src/db/DBconnection.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class DBconnection {
 	private static final String url="jdbc:mysql://localhost:3306/student_db";
 	private static final String user="root";
-	private static final String pass="manju2512";
+	private static final String pass="your_passward";
 	
 	public static Connection getConnection() throws SQLException{
 		return DriverManager.getConnection(url,user,pass);
